a97e907520c23ef94eb43ec5009e37836f4d866e
fawnbradshaw <fbradsha@chicagobooth.edu>
2023-02-11 01:24:16 +0000

    Dashboard: conversion link

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,59 @@
+class CurrenciesController < ApplicationController
+
+def first_currency
+
+  @raw_data = open("https://api.exchangerate.host/symbols").read
+
+  @parsed_data = JSON.parse(@raw_data)
+  @symbols_hash = @parsed_data.fetch("symbols")
+
+  @array_of_symbols = @symbols_hash.keys
+
+
+  render({ :template => "currency_templates/step_one.html.erb"})
+  
+end
+
+
+
+def second_currency
+
+  @raw_data = open("https://api.exchangerate.host/symbols").read
+
+  @parsed_data = JSON.parse(@raw_data)
+  @symbols_hash = @parsed_data.fetch("symbols")
+
+  @array_of_symbols = @symbols_hash.keys
+
+  @from_symbol = params.fetch("from_currency")
+
+
+
+  render({ :template => "currency_templates/step_two.html.erb"})
+  
+end
+
+def conversion
+
+  @from_symbol = params.fetch("from_currency")
+  @to_symbol = params.fetch("to_currency")
+  
+
+  @conversion_data = open("https://api.exchangerate.host/convert?from=" + @from_symbol+"&to="+@to_symbol).read
+
+
+  @parsed_data = JSON.parse(@conversion_data)
+
+  @conversion_hash = @parsed_data.fetch("info")
+
+  @array_of_conversions = @conversion_hash.keys
+
+  @conversion_amount = params.fetch("rate")
+
+
+
+  render({ :template => "currency_templates/conversion.html.erb"})
+  
+end
+
+end
